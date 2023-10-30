58f0a7adc6bc911ac3cdd27958df38d6e80ff27c
OwendB1 <owendebree@gmail.com>
2023-10-30 15:45:39 +0100

    First project setup + changes on C2 diagram

---
+++ srcs/ICAP_MarketService/Dtos.cs
@@ -0,0 +1,9 @@
+﻿namespace ICAP_MarketService
+{
+    public record ListingDto(Guid Id, string Name, string Description, bool IsActiveListing, decimal Price, 
+        DateTimeOffset CreatedDate);
+
+    public record CreateListingDto(string Name, string Description, bool IsActiveListing, decimal Price);
+
+    public record UpdateListingDto(string Name, string Description, bool IsActiveListing, decimal Price);
+}
