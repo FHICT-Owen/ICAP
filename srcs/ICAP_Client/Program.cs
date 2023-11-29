8488e139c48bd82d94fdff63f8d9b34b02aa18e0
OwendB1 <owendebree@gmail.com>
2023-11-29 04:33:27 +0100

    Add start of login for blazor client

--- srcs/ICAP_Client/Program.cs
+++ srcs/ICAP_Client/Program.cs
@@ -9,5 +9,10 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddMudServices();
+builder.Services.AddMsalAuthentication(options =>
+{
+    builder.Configuration.Bind("AzureAd", options.ProviderOptions.Authentication);
+    options.ProviderOptions.DefaultAccessTokenScopes.Add("https://graph.microsoft.com/User.Read");
+});
 
 await builder.Build().RunAsync();
