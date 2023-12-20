9486bc4d33ae7741b539faeee19e3232322a7ab8
OwendB1 <owendebree@gmail.com>
2023-12-20 11:20:37 +0100

    Update rules for authentication

--- srcs/ICAP_Client/Program.cs
+++ srcs/ICAP_Client/Program.cs
@@ -13,7 +13,17 @@ builder.Services.AddMsalAuthentication(options =>
 {
     builder.Configuration.Bind("AzureAd", options.ProviderOptions.Authentication);
     options.ProviderOptions.LoginMode = "redirect";
-    options.ProviderOptions.DefaultAccessTokenScopes.Add("https://graph.microsoft.com/User.Read");
+});
+
+builder.Services.AddAuthorizationCore(options =>
+{
+    options.AddPolicy("icap_admins", policy =>
+    {
+        policy.RequireAssertion(context => context.User.HasClaim(c =>
+        {
+            return c.Type == "groups" && c.Value.Contains(builder.Configuration["groups:icap_admins"]);
+        }));
+    });
 });
 
 await builder.Build().RunAsync();
