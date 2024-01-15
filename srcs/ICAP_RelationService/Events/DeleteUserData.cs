803a2b359b7ffd1d9b0c4f595a7a09daffefa2cc
OwendB1 <owendebree@gmail.com>
2024-01-15 19:35:34 +0100

    Switch out simple types

--- srcs/ICAP_RelationService/Events/DeleteUserData.cs
+++ srcs/ICAP_RelationService/Events/DeleteUserData.cs
@@ -5,25 +5,15 @@ using MassTransit;
 
 namespace ICAP_RelationService.Events
 {
-    public class DeleteUserData : IConsumer<string>
+    public record DeleteUserData(string UserId);
+    public class DeleteUserDataEvent(IRepository<Friends> friendRepository, IRepository<FriendRequest> friendRequestRepository) : IConsumer<DeleteUserData>
     {
-        private readonly IRepository<Friends> _friendRepository;
-        private readonly IRepository<FriendRequest> _friendRequestRepository;
-        public DeleteUserData(IRepository<Friends> friendRepository, IRepository<FriendRequest> friendRequestRepository)
-        {
-            _friendRepository = friendRepository;
-            _friendRequestRepository = friendRequestRepository;
-        }
-
-        public async Task Consume(ConsumeContext<string> ctx)
+        public async Task Consume(ConsumeContext<DeleteUserData> ctx)
         {
             Console.WriteLine($"Removing data for user {ctx.Message}");
-            await _friendRepository.RemoveAsync(ctx.Message);
-            var requestsToAndFromUser = await _friendRequestRepository.GetAllAsync(request => request.UserFrom == ctx.Message && request.UserTo == ctx.Message);
-            foreach (var request in requestsToAndFromUser)
-            {
-                await _friendRequestRepository.RemoveAsync(request.Id);
-            }
+            await friendRepository.RemoveAsync(ctx.Message.UserId);
+            await friendRequestRepository.RemoveAsync(filter =>
+                filter.UserFrom == ctx.Message.UserId || filter.UserTo == ctx.Message.UserId);
         }
     }
 }
